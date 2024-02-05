395e8dfec985d0952c8c201f2b96a23621f53bd3
Rishi Mishra <rm43084@gmail.com>
2024-02-05 19:46:29 +0530

    Organization

--- src/views/organization/organization.js
+++ src/views/organization/organization.js
@@ -61,74 +61,6 @@ const organization = () => {
 
   </CCardBody>
 
-  
-    <CCol xs={12}>
-      <CCard className="mb-2 container-div">
-        <CCardBody>
-        {/* <CDropdown>
-  <CDropdownToggle color="secondary">Job No.</CDropdownToggle>
-  <CDropdownMenu>
-    <CDropdownItem href="#">BE No.</CDropdownItem>
-    <CDropdownItem href="#">HBL/HAWB No.</CDropdownItem>
-    <CDropdownItem href="#">MBL/MAWB No.</CDropdownItem>
-    <CDropdownItem href="#">Container No.</CDropdownItem>
-  </CDropdownMenu>
-    <CFormInput type="text" size="sm" placeholder="" aria-label="sm input example"/>
-  </CDropdown> */}
-
-  <input type="text" placeholder="Job No."  className='text-field'/>
-
-  <CFormLabel className="col-sm-1 col-form-label ">Mode</CFormLabel>
-    <CDropdown className="col-sm-1 dropdown-button">
-  <CDropdownToggle color="secondary">Both</CDropdownToggle>
-  <CDropdownMenu>
-    <CDropdownItem href="#">Air</CDropdownItem>
-    <CDropdownItem href="#">Sea</CDropdownItem>
-  </CDropdownMenu>
-  </CDropdown>
-
-  <CFormLabel className="col-sm-1 col-form-label">BE Type</CFormLabel>
-    <CDropdown className="col-sm-1 dropdown-button">
-  <CDropdownToggle color="secondary">All</CDropdownToggle>
-  <CDropdownMenu>
-    <CDropdownItem href="#">Home</CDropdownItem>
-    <CDropdownItem href="#">In-Bond</CDropdownItem>
-    <CDropdownItem href="#">In-Bond</CDropdownItem>
-    <CDropdownItem href="#">SEZ-Z</CDropdownItem>
-    <CDropdownItem href="#">SEZ-M</CDropdownItem>
-    <CDropdownItem href="#">SEZ-T</CDropdownItem>
-
-  </CDropdownMenu>
-  </CDropdown>
-  <DatePicker
-        selectsStart
-        selected={startDate}
-        onChange={date => setStartDate(date)}
-        startDate={startDate}
-        className='col-form-label'
-      />
-
-  </CCardBody>
-  <div>
-  <DatePicker
-        selectsEnd
-        selected={endDate}
-        onChange={date => setEndDate(date)}
-        endDate={endDate}
-        startDate={startDate}
-        minDate={startDate}
-        className='col-form-label datepicker'
-     />
-  </div>
-  <div className='search-button'>
-  <CButton color="primary" type="submit">
-        Search
-      </CButton>
-  </div>
- 
-  </CCard>
-  </CCol>
-
   <CCol xs={12}>
       <CCard className="mb-2 container-div">
         <CCardBody>
@@ -143,13 +75,9 @@ const organization = () => {
     <CFormInput type="text" size="sm" placeholder="" aria-label="sm input example"/>
   </CDropdown> */}
 
-  <input type="text" placeholder="BE No."  className='text-field'/>
+  <input type="text" placeholder="Name"  className='text-field'/>
 
-  <input type="text" placeholder="HBL/HAWB No." className='text-field' />
-
-  <input type="text" placeholder="MBL/MAWB No." className='text-field' />
-
-  <input type="text" placeholder="Container No." className='text-field' />
+  <input type="text" placeholder="Alias" className='text-field' />
 
   </CCardBody>
   <div className='search-button'>
@@ -167,19 +95,8 @@ const organization = () => {
   <CTableHead>
     <CTableRow color='dark' >
       <CTableHeaderCell scope="col"></CTableHeaderCell>
-      <CTableHeaderCell scope="col">Date</CTableHeaderCell>
-      <CTableHeaderCell scope="col">Job No.</CTableHeaderCell>
-      <CTableHeaderCell scope="col">Importer Name</CTableHeaderCell>
-      <CTableHeaderCell scope="col">HBL/HAWB No.</CTableHeaderCell>
-      <CTableHeaderCell scope="col">MBL/MAWB No.</CTableHeaderCell>
-      <CTableHeaderCell scope="col">ETA</CTableHeaderCell>
-      <CTableHeaderCell scope="col">IGM</CTableHeaderCell>
-      <CTableHeaderCell scope="col">Checklist</CTableHeaderCell>
-      <CTableHeaderCell scope="col">E-Sanchit</CTableHeaderCell>
-      <CTableHeaderCell scope="col">BOE Filling</CTableHeaderCell>
-      <CTableHeaderCell scope="col">DO</CTableHeaderCell>
-      <CTableHeaderCell scope="col">Delivery</CTableHeaderCell>
-      <CTableHeaderCell scope="col">Remarks</CTableHeaderCell>
+      <CTableHeaderCell scope="col">Name</CTableHeaderCell>
+      <CTableHeaderCell scope="col">Alias</CTableHeaderCell>
 
     </CTableRow>
   </CTableHead>
@@ -193,37 +110,8 @@ const organization = () => {
                         Edit
                     </a>   */}
                   </th>
-      <CTableHeaderCell scope="row">06.09.2023 13:44:55</CTableHeaderCell>
-      <CTableDataCell>S/I/0001/23-24</CTableDataCell>
-      <CTableDataCell>PERMANENT MAGNET LTD</CTableDataCell>
-      <CTableDataCell>RTTT5787088</CTableDataCell>
-      <CTableDataCell>RTTT5787088</CTableDataCell>
-      <CTableDataCell>19-06-2023</CTableDataCell>
-      <CTableDataCell>19-06-2023 16:37:00</CTableDataCell>
-      <CTableDataCell>19-06-2023 16:37:00</CTableDataCell>
-      <CTableDataCell>19-06-2023 16:37:00</CTableDataCell>
-      <CTableDataCell>19-06-2023 16:37:00</CTableDataCell>
-      <CTableDataCell>19-06-2023 16:37:00</CTableDataCell>
-      <CTableDataCell>19-06-2023 16:37:00</CTableDataCell>
-      <CTableDataCell>Assesment</CTableDataCell>
-    </CTableRow>
-    <CTableRow>
-    <th scope="row" class="px-4 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-        <a href="/dashboard/import/Modal" target="_blank" rel="noopener noreferrer">Edit</a>  
-    </th>
-    <CTableHeaderCell scope="row">06.09.2023 13:44:55</CTableHeaderCell>
-      <CTableDataCell>A/I/0001/23-24</CTableDataCell>
-      <CTableDataCell>LASONS INDIA PVT LTD</CTableDataCell>
-      <CTableDataCell>RTTT5787088</CTableDataCell>
-      <CTableDataCell>RTTT5787088</CTableDataCell>
-      <CTableDataCell>19-06-2023</CTableDataCell>
-      <CTableDataCell>19-06-2023 16:37:00</CTableDataCell>
-      <CTableDataCell>19-06-2023 16:37:00</CTableDataCell>
-      <CTableDataCell>19-06-2023 16:37:00</CTableDataCell>
-      <CTableDataCell>19-06-2023 16:37:00</CTableDataCell>
-      <CTableDataCell>19-06-2023 16:37:00</CTableDataCell>
-      <CTableDataCell>19-06-2023 16:37:00</CTableDataCell>
-      <CTableDataCell>Done</CTableDataCell>
+      <CTableHeaderCell scope="row">Seawave Forwarding and Logistics Pvt. Ltd.</CTableHeaderCell>
+      <CTableDataCell>Prologis</CTableDataCell>
     </CTableRow>
     
   </CTableBody>
