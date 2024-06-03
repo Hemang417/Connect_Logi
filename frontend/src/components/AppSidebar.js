61895028919941fe2d34b375c4ad25928b158180
Rishi Mishra <rm43084@gmail.com>
2024-06-03 21:17:50 +0530

    Minor Changes - Logo Inserted

--- frontend/src/components/AppSidebar.js
+++ frontend/src/components/AppSidebar.js
@@ -7,7 +7,7 @@ import CIcon from '@coreui/icons-react'
 import { AppSidebarNav } from './AppSidebarNav'
 
 // import { logoNegative } from 'src/assets/brand/logo-negative'
-import logoNegativePng from 'src/images/connectlogi-logo-transparent.png'
+import logoNegativePng from 'src/images/connectlogi-logo-transparent-white.png'
 import { sygnet } from 'src/assets/brand/sygnet'
 
 import SimpleBar from 'simplebar-react'
